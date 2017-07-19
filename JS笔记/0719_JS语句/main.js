dac457c5d04174930d7a5e0592d54d52a4937e8c
Wph <2974769672@qq.com>
2017-07-19 13:13:08 +0800

    0719中午

---
+++ JS笔记/0719_JS语句/main.js
@@ -0,0 +1,23 @@
+
+
+var cars=["不是","是的","不是","可以是","真不可以是","真的可以的"];
+
+
+document.write("数组cars开始!<br>");
+document.write(cars[0] + "<br>");
+document.write(cars[1] + "<br>");
+document.write(cars[2] + "<br>");
+document.write(cars[3] + "<br>");
+document.write(cars[4] + "<br>");
+document.write(cars[5] + "<br>");
+//var sum;
+for(var sum=0, i=0;sum<10;sum+=2){
+    console.log(i);
+    i++;
+}
+var sum;
+console.log(i);
+console.log(sum);
+
+// var n=parseInt(prompt("请输入一个数，代码将为你判断是水仙花数，谢谢！"));
+// for()
