c45f2f4f0809eed6374ffedcef340b7236761c93
Wph <2974769672@qq.com>
2017-07-24 21:09:52 +0800

    0724结束

---
+++ JS笔记/0724_JS数组/main.js
@@ -0,0 +1,84 @@
+// console.log("yes");
+
+// console.log(a);// functicon a(){console.log(4);}
+// var a=1;//这一步已经把后面所有的函数定义都覆盖掉了
+// console.log(a);//1
+// function a(){console.log(2);}
+// console.log(a);//1
+// var a=3;
+// console.log(a);//3
+// function a(){console.log(4);}
+// console.log(a);//3
+
+// if (true) {
+// var color = "blue";
+// }
+// alert(color); //"blue"
+
+
+// var a=1;
+// function fun(a){
+//     console.log(a);
+//     a=2;
+// }
+// fun();//Undefined
+// console.log(a);//1
+
+// function setName(obj) {
+// obj.name = "Nicholas";
+// obj = new Object();
+// obj.name = "Greg";
+// }
+// var person = new Object();
+// setName(person);
+// alert(person.name); //"Nicholas"
+
+// var a=1;
+// function b(){
+//     console.log(this.b);
+// }
+// b();
+// console.log(a);
+
+// var a=[21,12,2,4,55,36,"s","A","2",71,8,100];
+// var b=["z","a","s","d","r","o","j"];
+// // console.log(a.toString());
+// // console.log(a.unshift(a.pop()));
+// // console.log(a);
+// // console.log(a.reverse());
+// // console.log(a.join("是"));
+// // console.log(a);
+// // console.log(a.toString());
+// // console.log(a);
+// // console.log(a.valueOf());
+// // console.log(a);
+// // console.log(a.sort());
+// // console.log(a);
+// // console.log(a.splice(2));
+// // console.log(a);
+
+// console.log(a.sort(function (a,b){return b-a;}));
+// console.log(b.sort(function (a,b){return b-a;}));
+
+// var a="asdfdsaGsadfEwqrqewr";
+// console.log(a.concat("是的","shide"));
+// console.log(a.charAt(5));
+// console.log(a.indexOf("s"));
+// console.log(a.replace("a","0"));
+// console.log(a);
+// console.log(a.slice(4,10));
+// console.log(a.split("a"));
+// console.log(a.toUpperCase());
+// console.log(a.toLowerCase());
+// console.log(a.charAt(0).toUpperCase()+a.slice(1));
+// console.log(a.replace("a","A"));
+
+
+document.title="翁鹏辉";
+
+var oSon=document.getElementById("son");
+oSon.style.width="100px";
+oSon.style.height="100px";
+oSon.style.backgroundColor="red";
+oSon.style.border="2px solid #000";
+oSon.style.margin="10px";
