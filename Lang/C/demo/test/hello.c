bfff51628610f08c298b67a4e5b7e7d8abe3d386
WPH <wengpenghui@abcpen.com>
2018-07-01 15:52:58 +0800

    fix

--- Lang/C/demo/test/hello.c
+++ Lang/C/demo/test/hello.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int g_var = 0;
+
 void print_line(char *str)
 {
     if (str != NULL)
@@ -7,10 +8,12 @@ void print_line(char *str)
     else
         printf("null string\r\n");
 }
+
 int main(int argc, char **argv)
 {
     int l_var = 1;
     print_line("hello world!");
     printf("g_var = %d, l_var = %d.\r\n", g_var, l_var);
+    scanf("%d");
     return 0;
 }
